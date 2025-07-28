51cb08e5b163b4b41b72bca69827973c0a02365c
gokuljs <gokuljs029@gmail.com>
2025-07-28 21:01:47 +0530

    Rime initial setup

--- 01-say-one-thing.py
+++ 01-say-one-thing.py
@@ -6,11 +6,12 @@ from dotenv import load_dotenv
 from pipecat.transports.services.daily import DailyParams
 from pipecat.transports.network.fastapi_websocket import FastAPIWebsocketParams
 from pipecat.transports.base_transport import BaseTransport, TransportParams
-from pipecat.services.cartesia.tts import CartesiaTTSService
+from pipecat.services.rime.tts import RimeTTSService
 from pipecat.pipeline.task import PipelineTask
 from pipecat.pipeline.runner import PipelineRunner
 from pipecat.pipeline.pipeline import Pipeline
 from pipecat.frames.frames import EndFrame, TTSSpeakFrame
+from pipecat.transcriptions.language import Language
 
 # Configure logger
 logger = logging.getLogger("rime-pipecat")
@@ -33,18 +34,37 @@ transport_params = {
 async def run_example(transport: BaseTransport, _: argparse.Namespace, handle_sigint: bool):
     logger.info("Starting a bot that says one thing")
 
-    tts = CartesiaTTSService(
-        api_key=os.getenv("CARTESIA_API_KEY"),
-        voice_id="71a7ad14-091c-4e8e-a314-022ece01c121",  # British Reading Lady
-    )
+    logger.info("Rime API Key: %s", os.getenv("RIME_API_KEY"))
+    logger.info("Rime tts initialization")
+    tts = RimeTTSService(
+        api_key=os.getenv("RIME_API_KEY"),
+        voice_id="rex",
+        model="mistv2",
+        url="wss://users.rime.ai/ws2",
+        params=RimeTTSService.InputParams(
+            language=Language.EN,
+            speed_alpha=1.0,
+            reduce_latency=False,
+            pause_between_brackets=True,
+            phonemize_between_brackets=False
 
+        )
+    )
+    logger.info("pipeline Setup")
+    # PipelineTask is the central orchestrator that manages pipeline execution, frame routing, and lifecycle events
+    # Pipeline is the actual chain of frame processors (like TTS, LLM, STT services) connected in sequence
     task = PipelineTask(Pipeline([tts, transport.output()]))
 
     # Register an event handler so we can play the audio when the client joins
     @transport.event_handler("on_client_connected")
     async def on_client_connected(transport, client):
-        await task.queue_frames([TTSSpeakFrame(f"Hello there!"), EndFrame()])
-
+        # The queue_frames() method allows you to inject frames into the pipeline for processing:
+        await task.queue_frames([TTSSpeakFrame("Welcome! This is a demonstration of Rime's Text-to-Speech capabilities. The voice you're hearing is generated in real-time using advanced AI technology."),
+                                 TTSSpeakFrame(
+                                     "We are using pipecat to build a bot that can say one thing"),
+                                 EndFrame()])
+    # PipelineRunner is the high-level execution manager that runs pipeline tasks with lifecycle and signal handling .
+    # The handle_sigint parameter controls whether PipelineRunner automatically handles system interrupt signals (SIGINT and SIGTERM) for graceful shutdown , Resource Cleanup:
     runner = PipelineRunner(handle_sigint=handle_sigint)
 
     await runner.run(task)
